3ae5626683dd89d75ca1eba9de9d0d586d8c80fd
Deon Nicholas <dojiboy9@hotmail.com>
2014-10-08 10:33:00 -0400

    Moved bootstrap back to /lib directory

--- index.php
+++ index.php
@@ -56,7 +56,7 @@
 	
 	
     <!-- Library javascript files -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="js/lib/bootstrap.min.js"></script>
 	<script src="js/lib/underscore-min.js"></script>
 	<script src="js/lib/backbone-min.js"></script>
   </body>
